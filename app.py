6087af26343b0e7003edd4fcbeb9cae8234de173
Juhi Dungrani <juhi@toystack.co>
2024-02-29 10:09:26 +0530

    added toystack app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!I am new python application'
+    return 'Hello, World!I am new python Toystack application'
 
 if __name__ == '__main__':
     app.run(debug=True)
